05f8c34159518b11a49e3f463c1a0411f28ceeec
Sarumi sodiq <sodiq2b2@gmail.com>
2024-04-11 17:16:29 -0700

    remove old readme file

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-// Script that prints "JavaScript is amazing".
+// Script that prints "JavaScript is amazing". i love it
 
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
--- 0x12-javascript-warm_up/Readme.md
+++ 0x12-javascript-warm_up/Readme.md
@@ -1,1 +1,1 @@
-Readme file for javascript warm up
+
